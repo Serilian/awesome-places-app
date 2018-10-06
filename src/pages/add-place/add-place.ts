87e8c7138f32a4dcc3fefc0bcfd10be4955d11a9
Filip <fhagno@yahoo.com>
2018-10-06 16:33:58 +0200

    Adding locate me functionality

--- src/pages/add-place/add-place.ts
+++ src/pages/add-place/add-place.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import {IonicPage, ModalController} from 'ionic-angular';
+import {IonicPage, LoadingController, ModalController, ToastController} from 'ionic-angular';
 import {NgForm} from "@angular/forms";
 import {SetLocationPage} from "../set-location/set-location";
 import {Location} from "../../models/location";
+import { Geolocation } from '@ionic-native/geolocation';
 
 @IonicPage()
 @Component({
@@ -18,7 +19,7 @@ export class AddPlacePage {
 
   isLocationSet = false;
 
-  constructor(private modalCtrl: ModalController) {
+  constructor(private geolocation: Geolocation, private modalCtrl: ModalController, private loadingCtrl: LoadingController, private toastCtrl: ToastController) {
   }
 
   ionViewDidLoad() {
@@ -38,6 +39,31 @@ export class AddPlacePage {
         this.isLocationSet = true;
       }
     });
+  }
+
+  onLocate() {
+    const loader = this.loadingCtrl.create({
+      content: 'Fetching your location...'
+    });
+    loader.present();
+    const toast = this.toastCtrl.create({
+      message: 'Error fetching data',
+      closeButtonText: 'Ok',
+      duration: 2500
+    });
+    this.geolocation.getCurrentPosition()
+      .then(location => {
+        console.log(location);
+        this.location.lat = location.coords.latitude;
+        this.location.lgn = location.coords.longitude;
+        this.isLocationSet = true;
+        loader.dismiss();
+      })
+      .catch((error) => {
+        console.log(error);
+        loader.dismiss();
+        toast.present();
+      });
   }
 
 }
