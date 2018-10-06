0bae6b634432fe0459b7a67a4b368993a32cd4ab
Filip <fhagno@yahoo.com>
2018-10-06 09:13:49 +0200

    Adding select location.

--- src/pages/add-place/add-place.ts
+++ src/pages/add-place/add-place.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {IonicPage, ModalController} from 'ionic-angular';
 import {NgForm} from "@angular/forms";
 import {SetLocationPage} from "../set-location/set-location";
+import {Location} from "../../models/location";
 
 @IonicPage()
 @Component({
@@ -9,6 +10,13 @@ import {SetLocationPage} from "../set-location/set-location";
   templateUrl: 'add-place.html',
 })
 export class AddPlacePage {
+
+  location: Location = {
+    lat: 50.334900,
+    lgn: 19.561120
+  };
+
+  isLocationSet = false;
 
   constructor(private modalCtrl: ModalController) {
   }
@@ -22,8 +30,14 @@ export class AddPlacePage {
   }
 
   onOpenMap() {
-    const modal = this.modalCtrl.create(SetLocationPage);
+    const modal = this.modalCtrl.create(SetLocationPage, {location: this.location, isSet: this.isLocationSet});
     modal.present();
+    modal.onDidDismiss((data)=> {
+      if (data) {
+        this.location = data.location;
+        this.isLocationSet = true;
+      }
+    });
   }
 
 }
